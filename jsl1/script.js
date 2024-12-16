d1065bebe13275f24456f68081adfa448379733c
animeroman <163325219+animeroman@users.noreply.github.com>
2024-12-16 11:50:37 +0400

    Adding jsl

---
+++ jsl1/script.js
@@ -0,0 +1,88 @@
+/*
+console.log(typeof null);
+console.log(typeof 31);
+console.log(typeof "aydin");
+console.log(typeof true);
+console.log(typeof abc);
+let AydinCupu = '19mm';
+console.log(AydinCupu);
+let SeidCupu = '33m'
+console.log(SeidCupu)
+let age =90
+age =50
+console.log(age)
+const boy = 190
+// boy = 150
+console.log(boy)
+
+const now = 1028
+const ageSaid = now - 2007
+const agejesus = now - 1
+console.log(ageSaid, agejesus)
+console.log(ageSaid*(-1), (agejesus-1) / 2,3**3)
+const FirstName='tosu'
+const LastName='zengilasnki'
+console.log(FirstName+"",LastName);
+*/
+/*
+const massMark = 78
+const heightMark = 1.69
+const massJohn = 92
+const heightJohn = 1.95
+
+const BMIMark = massMark / heightMark ** 2
+const BMIJohn = massJohn / heightJohn ** 2
+
+console.log(BMIMark > BMIJohn)
+
+const massMark2 = 95
+const heightMark2 = 1.88
+const massJohn2 = 85
+const heightJohn2 = 1.76
+
+const BMIMark2 = massMark2 / heightMark2 ** 2
+const BMIJohn2 = massJohn2 / heightJohn2 ** 2
+
+console.log(BMIMark2 > BMIJohn2);
+*/
+/*
+const now = 2024
+const saidagee = now - 2007
+console.log(saidagee)
+
+const aaabbb = `just a regular isitrings??`
+console.log(aaabbb)
+console.log(`i \n\
+learn \n\
+lines \n\
+mff`);
+
+const thinG = "/n/"
+
+console.log(`fuck 
+this
+${thinG}
+shit`)
+*/
+/* const Hisage = 20
+if (Hisage>=18){
+console.log(`iyi uçuşlar..Allahu ekber!✈💥🏰`)
+} else{
+    console.log(`fuck this shit uçağa bin..Allahu ekber!✈💥🏰`)
+}
+*/
+/*
+const massMark = 78
+const heightMark = 1.69
+const massJohn = 92
+const heightJohn = 1.95
+
+const BMIMark = massMark / heightMark ** 2
+const BMIJohn = massJohn / heightJohn ** 2
+
+if (BMIMark > BMIJohn){
+    console.log(`Mark's BMI is higher than John's!`)
+} else {console.log (`John's BMI is higher than Mark's`)}
+*/
+// const Birthage=2007
+// console.log(`11.12.` + String(Birthage));
